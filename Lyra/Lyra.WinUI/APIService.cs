3b958b1b0b1e728e728a7dbc6048f38b59b86bdf
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-03-12 13:28:32 +0100

    [Add] Simple Windows Forms for research

---
+++ Lyra/Lyra.WinUI/APIService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Flurl;
+using Lyra.Model;
+
+namespace Lyra.WinUI
+{
+    public class APIService
+    {
+        private readonly string _route;
+
+        public APIService(string route)
+        {
+            _route = route;
+        }
+
+        public async Task<T> Get<T>(object search)
+        {
+            var url = $"{Properties.Settings.Default.APIUrl}/{_route}";
+
+            if(search != null)
+            {
+                url += "?";
+                url += await search.ToQueryString();
+            }
+
+            return await url.GetJsonAsync<T>();
+        }
+    }
+}
---
+++ Lyra/Lyra.WinUI/Track/frmTrack.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Lyra.WinUI.Track
+{
+    partial class frmTrack
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvTracks = new System.Windows.Forms.DataGridView();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTracks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgvTracks);
+            this.groupBox1.Location = new System.Drawing.Point(12, 69);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 369);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tracks";
+            // 
+            // dgvTracks
+            // 
+            this.dgvTracks.AllowUserToAddRows = false;
+            this.dgvTracks.AllowUserToDeleteRows = false;
+            this.dgvTracks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTracks.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTracks.Location = new System.Drawing.Point(3, 16);
+            this.dgvTracks.Name = "dgvTracks";
+            this.dgvTracks.ReadOnly = true;
+            this.dgvTracks.Size = new System.Drawing.Size(770, 350);
+            this.dgvTracks.TabIndex = 0;
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(696, 40);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(89, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(15, 43);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // frmTrack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmTrack";
+            this.Text = "frmTrack";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTracks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvTracks;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
