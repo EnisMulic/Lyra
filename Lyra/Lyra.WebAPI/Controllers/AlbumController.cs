ad1005c6d0c745c422f340a902e6b3d5cad70fcf
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-03-18 09:33:22 +0100

    [Update] Album services and controllers to inherit from generic service and controller

--- Lyra/Lyra.WebAPI/Controllers/AlbumController.cs
+++ Lyra/Lyra.WebAPI/Controllers/AlbumController.cs
@@ -10,44 +10,10 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Lyra.WebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AlbumController : ControllerBase
+    public class AlbumController : BaseController<Album, AlbumSearchRequest>
     {
-        private readonly IAlbumService _service;
-        public AlbumController(IAlbumService service)
+        public AlbumController(IBaseService<Album, AlbumSearchRequest> service) : base(service)
         {
-            _service = service;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Album>> Get([FromQuery]AlbumSearchRequest request)
-        {
-            return Ok(_service.Get(request));
-        }
-
-        [HttpGet("{id}")]
-        public ActionResult<Album> GetById(int id)
-        {
-            return _service.GetById(id);
-        }
-
-        [HttpPost]
-        public Album Insert(AlbumInsertRequest request)
-        {
-            return _service.Insert(request);
-        }
-
-        [HttpPut("{id}")]
-        public Album Update(int id, Album Album)
-        {
-            return null;
-        }
-
-        [HttpDelete("{id}")]
-        public Album Delete(int id)
-        {
-            return null;
         }
     }
 }
--- Lyra/Lyra.WebAPI/Services/AlbumService.cs
+++ Lyra/Lyra.WebAPI/Services/AlbumService.cs
@@ -9,17 +9,18 @@ using System.Threading.Tasks;
 
 namespace Lyra.WebAPI.Services
 {
-    public class AlbumService : IAlbumService
+    public class AlbumService : BaseService<Model.Album, AlbumSearchRequest, Database.Album>
     {
         private readonly LyraContext _context;
         private readonly IMapper _mapper;
 
-        public AlbumService(LyraContext context, IMapper mapper)
+        public AlbumService(LyraContext context, IMapper mapper) : base(context, mapper)
         {
             _context = context;
             _mapper = mapper;
         }
-        public IList<Model.Album> Get(AlbumSearchRequest request)
+
+        public override List<Model.Album> Get(AlbumSearchRequest request)
         {
             var query = _context.Albums.AsQueryable();
 
@@ -28,26 +29,13 @@ namespace Lyra.WebAPI.Services
                 query = query.Where(x => x.Name.StartsWith(request.Name));
             }
 
-            
+
 
             var list = query.ToList();
 
             return _mapper.Map<List<Model.Album>>(list);
         }
 
-        public Model.Album GetById(int id)
-        {
-            var item = _context.Albums.Find(id);
-            return _mapper.Map<Model.Album>(item);
-        }
-
-        public Model.Album Insert(AlbumInsertRequest request)
-        {
-            var entity = _mapper.Map<Database.Album>(request);
-            _context.Albums.Add(entity);
-            _context.SaveChanges();
-
-            return _mapper.Map<Model.Album>(entity);
-        }
+        
     }
 }
