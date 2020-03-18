cc23dad833b3943ad6fc05ea85fb88b5d2329687
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-03-18 09:33:43 +0100

    [Add] Artist to api

---
+++ Lyra/Lyra.WebAPI/Controllers/ArtistController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lyra.WebAPI.Services;
+using Lyra.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lyra.WebAPI.Controllers
+{
+    
+    public class ArtistController : CRUDController<Artist, object, object, object>
+    {
+        public ArtistController(ICRUDService<Artist, object, object, object> service) : base(service)
+        {
+        }
+    }
+}
---
+++ Lyra/Lyra.WebAPI/Services/ArtistService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Lyra.WebAPI.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lyra.WebAPI.Services
+{
+    public class ArtistService : CRUDService<Model.Artist, object, Database.Artist, object, object>
+    {
+        public ArtistService(LyraContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
