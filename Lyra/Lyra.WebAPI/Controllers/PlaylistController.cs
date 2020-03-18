220d8b2104cee1f1cf35b2c379de289edc215488
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-03-18 09:33:58 +0100

    [Add] Playlist to api

---
+++ Lyra/Lyra.WebAPI/Controllers/PlaylistController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lyra.Model;
+using Lyra.WebAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lyra.WebAPI.Controllers
+{
+    public class PlaylistController : CRUDController<Model.Playlist, object, object, object>
+    {
+        public PlaylistController(ICRUDService<Playlist, object, object, object> service) : base(service)
+        {
+        }
+    }
+}
---
+++ Lyra/Lyra.WebAPI/Services/PlaylistService.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Lyra.WebAPI.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lyra.WebAPI.Services
+{
+    public class PlaylistService : CRUDService<Model.Playlist, object, Database.Playlist, object, object>
+    {
+        public PlaylistService(LyraContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
