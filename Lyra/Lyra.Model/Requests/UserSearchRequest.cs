5e5e2dad780dc60d282b1ae0166b5dcf8d0ab092
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-03-31 20:09:03 +0200

    Update UserSearchRequest for username/email/phone searches

--- Lyra/Lyra.Model/Requests/UserSearchRequest.cs
+++ Lyra/Lyra.Model/Requests/UserSearchRequest.cs
@@ -8,6 +8,8 @@ namespace Lyra.Model.Requests
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
     }
 }
