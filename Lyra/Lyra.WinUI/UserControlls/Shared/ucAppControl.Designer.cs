91b656ac7ab93e844afd15e49e12d32164d7f546
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-04-07 12:53:02 +0200

    Add UserControll for minimize, maximize and close

---
+++ Lyra/Lyra.WinUI/UserControlls/Shared/ucAppControl.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Lyra.WinUI.UserControlls.Shared
+{
+    partial class ucAppControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnMaximized = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(4, 4);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(30, 26);
+            this.btnMinimize.TabIndex = 7;
+            this.btnMinimize.Text = "X";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(76, 4);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(30, 26);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMaximized
+            // 
+            this.btnMaximized.Location = new System.Drawing.Point(40, 4);
+            this.btnMaximized.Name = "btnMaximized";
+            this.btnMaximized.Size = new System.Drawing.Size(30, 26);
+            this.btnMaximized.TabIndex = 6;
+            this.btnMaximized.Text = "X";
+            this.btnMaximized.UseVisualStyleBackColor = true;
+            this.btnMaximized.Click += new System.EventHandler(this.btnMaximized_Click);
+            // 
+            // ucAppControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnMinimize);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMaximized);
+            this.Name = "ucAppControl";
+            this.Size = new System.Drawing.Size(110, 35);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnMaximized;
+    }
+}
---
+++ Lyra/Lyra.WinUI/UserControlls/Shared/ucAppControl.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lyra.WinUI.UserControlls.Shared
+{
+    public partial class ucAppControl : UserControl
+    {
+        private static ucAppControl _instance;
+        public static ucAppControl Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new ucAppControl();
+                return _instance;
+            }
+        }
+        public ucAppControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            ParentForm.Close();
+        }
+
+        private void btnMaximized_Click(object sender, EventArgs e)
+        {
+            if (ParentForm.WindowState == FormWindowState.Maximized)
+            {
+                ParentForm.WindowState = FormWindowState.Normal;
+            }
+            else if (ParentForm.WindowState == FormWindowState.Normal)
+            {
+                ParentForm.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            ParentForm.WindowState = FormWindowState.Minimized;
+        }
+    }
+}
