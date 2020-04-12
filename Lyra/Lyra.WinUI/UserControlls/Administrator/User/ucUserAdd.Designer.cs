bb956fd20f0870d2cd83334805b04b4052d31919
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-04-12 16:18:21 +0200

    Redesing and implement User insertion form

--- Lyra/Lyra.WinUI/UserControlls/Administrator/User/ucUserAdd.Designer.cs
+++ Lyra/Lyra.WinUI/UserControlls/Administrator/User/ucUserAdd.Designer.cs
@@ -45,13 +45,18 @@
             this.txtConfirmPassword = new System.Windows.Forms.TextBox();
             this.pictureBoxUserImage = new System.Windows.Forms.PictureBox();
             this.btnUploadImage = new System.Windows.Forms.Button();
+            this.clbRoles = new System.Windows.Forms.CheckedListBox();
+            this.lblUserInfo = new System.Windows.Forms.Label();
+            this.lblLine1 = new System.Windows.Forms.Label();
+            this.lblLine2 = new System.Windows.Forms.Label();
+            this.lblUserRoles = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUserImage)).BeginInit();
             this.SuspendLayout();
             // 
             // lblPhoneNumber
             // 
             this.lblPhoneNumber.AutoSize = true;
-            this.lblPhoneNumber.Location = new System.Drawing.Point(41, 382);
+            this.lblPhoneNumber.Location = new System.Drawing.Point(294, 252);
             this.lblPhoneNumber.Name = "lblPhoneNumber";
             this.lblPhoneNumber.Size = new System.Drawing.Size(75, 13);
             this.lblPhoneNumber.TabIndex = 45;
@@ -59,31 +64,33 @@
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(44, 398);
+            this.txtPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhoneNumber.Location = new System.Drawing.Point(297, 268);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(188, 20);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(188, 22);
             this.txtPhoneNumber.TabIndex = 44;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
-            this.lblEmail.Location = new System.Drawing.Point(277, 329);
+            this.lblEmail.Location = new System.Drawing.Point(530, 190);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(57, 13);
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
             this.lblEmail.TabIndex = 43;
-            this.lblEmail.Text = "First Name";
+            this.lblEmail.Text = "Email";
             // 
             // txtEmail
             // 
-            this.txtEmail.Location = new System.Drawing.Point(280, 345);
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(533, 206);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(188, 20);
+            this.txtEmail.Size = new System.Drawing.Size(188, 22);
             this.txtEmail.TabIndex = 42;
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(41, 329);
+            this.lblUsername.Location = new System.Drawing.Point(294, 190);
             this.lblUsername.Name = "lblUsername";
             this.lblUsername.Size = new System.Drawing.Size(55, 13);
             this.lblUsername.TabIndex = 41;
@@ -91,15 +98,16 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(44, 345);
+            this.txtUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(297, 206);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(188, 20);
+            this.txtUsername.Size = new System.Drawing.Size(188, 22);
             this.txtUsername.TabIndex = 40;
             // 
             // lblLastName
             // 
             this.lblLastName.AutoSize = true;
-            this.lblLastName.Location = new System.Drawing.Point(277, 275);
+            this.lblLastName.Location = new System.Drawing.Point(530, 124);
             this.lblLastName.Name = "lblLastName";
             this.lblLastName.Size = new System.Drawing.Size(58, 13);
             this.lblLastName.TabIndex = 39;
@@ -107,15 +115,16 @@
             // 
             // txtLastName
             // 
-            this.txtLastName.Location = new System.Drawing.Point(280, 291);
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastName.Location = new System.Drawing.Point(533, 140);
             this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(188, 20);
+            this.txtLastName.Size = new System.Drawing.Size(188, 22);
             this.txtLastName.TabIndex = 38;
             // 
             // lblFirstName
             // 
             this.lblFirstName.AutoSize = true;
-            this.lblFirstName.Location = new System.Drawing.Point(41, 275);
+            this.lblFirstName.Location = new System.Drawing.Point(294, 124);
             this.lblFirstName.Name = "lblFirstName";
             this.lblFirstName.Size = new System.Drawing.Size(57, 13);
             this.lblFirstName.TabIndex = 37;
@@ -123,14 +132,15 @@
             // 
             // txtFirstName
             // 
-            this.txtFirstName.Location = new System.Drawing.Point(44, 291);
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.Location = new System.Drawing.Point(297, 140);
             this.txtFirstName.Name = "txtFirstName";
-            this.txtFirstName.Size = new System.Drawing.Size(188, 20);
+            this.txtFirstName.Size = new System.Drawing.Size(188, 22);
             this.txtFirstName.TabIndex = 36;
             // 
             // btnSave
             // 
-            this.btnSave.Location = new System.Drawing.Point(44, 491);
+            this.btnSave.Location = new System.Drawing.Point(44, 633);
             this.btnSave.Name = "btnSave";
             this.btnSave.Size = new System.Drawing.Size(188, 23);
             this.btnSave.TabIndex = 35;
@@ -141,7 +151,7 @@
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(41, 435);
+            this.lblPassword.Location = new System.Drawing.Point(294, 315);
             this.lblPassword.Name = "lblPassword";
             this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 47;
@@ -149,16 +159,17 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(44, 451);
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(297, 332);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(188, 20);
+            this.txtPassword.Size = new System.Drawing.Size(188, 22);
             this.txtPassword.TabIndex = 46;
             // 
             // lblConfirmPassword
             // 
             this.lblConfirmPassword.AutoSize = true;
-            this.lblConfirmPassword.Location = new System.Drawing.Point(277, 435);
+            this.lblConfirmPassword.Location = new System.Drawing.Point(530, 315);
             this.lblConfirmPassword.Name = "lblConfirmPassword";
             this.lblConfirmPassword.Size = new System.Drawing.Size(91, 13);
             this.lblConfirmPassword.TabIndex = 49;
@@ -166,16 +177,17 @@
             // 
             // txtConfirmPassword
             // 
-            this.txtConfirmPassword.Location = new System.Drawing.Point(280, 451);
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtConfirmPassword.Location = new System.Drawing.Point(533, 331);
             this.txtConfirmPassword.Name = "txtConfirmPassword";
             this.txtConfirmPassword.PasswordChar = '*';
-            this.txtConfirmPassword.Size = new System.Drawing.Size(188, 20);
+            this.txtConfirmPassword.Size = new System.Drawing.Size(188, 22);
             this.txtConfirmPassword.TabIndex = 48;
             // 
             // pictureBoxUserImage
             // 
             this.pictureBoxUserImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBoxUserImage.Location = new System.Drawing.Point(44, 13);
+            this.pictureBoxUserImage.Location = new System.Drawing.Point(48, 124);
             this.pictureBoxUserImage.Name = "pictureBoxUserImage";
             this.pictureBoxUserImage.Size = new System.Drawing.Size(188, 184);
             this.pictureBoxUserImage.TabIndex = 50;
@@ -183,7 +195,7 @@
             // 
             // btnUploadImage
             // 
-            this.btnUploadImage.Location = new System.Drawing.Point(44, 220);
+            this.btnUploadImage.Location = new System.Drawing.Point(48, 331);
             this.btnUploadImage.Name = "btnUploadImage";
             this.btnUploadImage.Size = new System.Drawing.Size(188, 23);
             this.btnUploadImage.TabIndex = 51;
@@ -191,10 +203,67 @@
             this.btnUploadImage.UseVisualStyleBackColor = true;
             this.btnUploadImage.Click += new System.EventHandler(this.btnUploadImage_Click);
             // 
+            // clbRoles
+            // 
+            this.clbRoles.FormattingEnabled = true;
+            this.clbRoles.Location = new System.Drawing.Point(48, 436);
+            this.clbRoles.Name = "clbRoles";
+            this.clbRoles.Size = new System.Drawing.Size(673, 154);
+            this.clbRoles.TabIndex = 52;
+            // 
+            // lblUserInfo
+            // 
+            this.lblUserInfo.AutoSize = true;
+            this.lblUserInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserInfo.Location = new System.Drawing.Point(39, 56);
+            this.lblUserInfo.Name = "lblUserInfo";
+            this.lblUserInfo.Size = new System.Drawing.Size(51, 25);
+            this.lblUserInfo.TabIndex = 53;
+            this.lblUserInfo.Text = "Info";
+            // 
+            // lblLine1
+            // 
+            this.lblLine1.AccessibleRole = System.Windows.Forms.AccessibleRole.Border;
+            this.lblLine1.AutoSize = true;
+            this.lblLine1.BackColor = System.Drawing.Color.Transparent;
+            this.lblLine1.Location = new System.Drawing.Point(41, 81);
+            this.lblLine1.Name = "lblLine1";
+            this.lblLine1.Size = new System.Drawing.Size(691, 13);
+            this.lblLine1.TabIndex = 54;
+            this.lblLine1.Text = "_________________________________________________________________________________" +
+    "_________________________________";
+            // 
+            // lblLine2
+            // 
+            this.lblLine2.AccessibleRole = System.Windows.Forms.AccessibleRole.Border;
+            this.lblLine2.AutoSize = true;
+            this.lblLine2.BackColor = System.Drawing.Color.Transparent;
+            this.lblLine2.Location = new System.Drawing.Point(41, 402);
+            this.lblLine2.Name = "lblLine2";
+            this.lblLine2.Size = new System.Drawing.Size(691, 13);
+            this.lblLine2.TabIndex = 56;
+            this.lblLine2.Text = "_________________________________________________________________________________" +
+    "_________________________________";
+            // 
+            // lblUserRoles
+            // 
+            this.lblUserRoles.AutoSize = true;
+            this.lblUserRoles.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserRoles.Location = new System.Drawing.Point(39, 377);
+            this.lblUserRoles.Name = "lblUserRoles";
+            this.lblUserRoles.Size = new System.Drawing.Size(72, 25);
+            this.lblUserRoles.TabIndex = 55;
+            this.lblUserRoles.Text = "Roles";
+            // 
             // ucUserAdd
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblLine2);
+            this.Controls.Add(this.lblUserRoles);
+            this.Controls.Add(this.lblLine1);
+            this.Controls.Add(this.lblUserInfo);
+            this.Controls.Add(this.clbRoles);
             this.Controls.Add(this.btnUploadImage);
             this.Controls.Add(this.pictureBoxUserImage);
             this.Controls.Add(this.lblConfirmPassword);
@@ -214,6 +283,7 @@
             this.Controls.Add(this.btnSave);
             this.Name = "ucUserAdd";
             this.Size = new System.Drawing.Size(796, 690);
+            this.Load += new System.EventHandler(this.ucUserAdd_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxUserImage)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -239,5 +309,10 @@
         private System.Windows.Forms.TextBox txtConfirmPassword;
         private System.Windows.Forms.PictureBox pictureBoxUserImage;
         private System.Windows.Forms.Button btnUploadImage;
+        private System.Windows.Forms.CheckedListBox clbRoles;
+        private System.Windows.Forms.Label lblUserInfo;
+        private System.Windows.Forms.Label lblLine1;
+        private System.Windows.Forms.Label lblLine2;
+        private System.Windows.Forms.Label lblUserRoles;
     }
 }
--- Lyra/Lyra.WinUI/UserControlls/Administrator/User/ucUserAdd.cs
+++ Lyra/Lyra.WinUI/UserControlls/Administrator/User/ucUserAdd.cs
@@ -13,15 +13,27 @@ namespace Lyra.WinUI.UserControlls.Administrator.User
     public partial class ucUserAdd : UserControl
     {
         private readonly APIService _apiService = new APIService("User");
+        private readonly APIService _roleApiService = new APIService("Role");
         public ucUserAdd()
         {
             InitializeComponent();
+            
+        }
+
+        private async void ucUserAdd_Load(object sender, EventArgs e)
+        {
+            var roles = await _roleApiService.Get<List<Model.Role>>(null);
+            clbRoles.DataSource = roles;
+            clbRoles.ValueMember = "ID";
+            clbRoles.DisplayMember = "Name";
         }
 
         private async void btnSave_Click(object sender, EventArgs e)
         {
             try
             {
+                var roleList = clbRoles.CheckedItems.Cast<Model.Role>().Select(i => i.ID).ToList();
+
                 var request = new Model.Requests.UserUpsertRequest
                 {
                     FirstName = Convert.ToString(txtFirstName.Text),
@@ -30,7 +42,8 @@ namespace Lyra.WinUI.UserControlls.Administrator.User
                     Email = Convert.ToString(txtEmail.Text),
                     PhoneNumber = Convert.ToString(txtPhoneNumber.Text),
                     Password = Convert.ToString(txtPassword.Text),
-                    PasswordConfirmation = Convert.ToString(txtConfirmPassword.Text)
+                    PasswordConfirmation = Convert.ToString(txtConfirmPassword.Text),
+                    Roles = roleList
                 };
 
                 await _apiService.Insert<Model.User>(request);
