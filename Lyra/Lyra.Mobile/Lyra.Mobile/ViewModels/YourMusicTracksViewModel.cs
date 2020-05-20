5c193255a152973e421df85a847cb7445464953c
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-05-20 21:01:31 +0200

    Add basic Your Music Track Page

---
+++ Lyra/Lyra.Mobile/Lyra.Mobile/ViewModels/YourMusicTracksViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Lyra.Mobile.Helpers;
+using Lyra.Mobile.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyra.Mobile.ViewModels
+{
+    public class YourMusicTracksViewModel : BaseViewModel
+    {
+        private readonly APIService _service = new APIService("");
+        public ObservableCollection<Model.Track> TracksList { get; set; } = new ObservableCollection<Model.Track>();
+        private bool IsLoaded = false;
+        public YourMusicTracksViewModel()
+        {
+        }
+
+        public async Task Init()
+        {
+            try
+            {
+                if (!IsLoaded)
+                {
+                    int ID = SignedInUserHelper.User.ID;
+
+                    var tracks = await _service.GetFavouriteTracks(ID);
+                    foreach (var track in tracks)
+                    {
+                        TracksList.Add(track);
+                    }
+
+                    IsLoaded = true;
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
