d69ce131380ca3224c9534e3169aa57e4c67b9f7
PancakeAlchemist <enis.s.mulic@gmail.com>
2020-06-06 22:22:57 +0200

    Add events for album, artist, playlist, track click

--- Lyra/Lyra.Mobile/Lyra.Mobile/Views/SearchPage.xaml.cs
+++ Lyra/Lyra.Mobile/Lyra.Mobile/Views/SearchPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Lyra.Mobile.ViewModels;
+﻿using Lyra.Mobile.Helpers;
+using Lyra.Mobile.Services;
+using Lyra.Mobile.ViewModels;
+using Lyra.Model;
+using Lyra.Model.Requests;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +17,7 @@ namespace Lyra.Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SearchPage : ContentPage
     {
+        private readonly APIService _loggingService = new APIService("User");
         private SearchViewModel model;
         public SearchPage(string search = null)
         {
@@ -20,24 +25,56 @@ namespace Lyra.Mobile.Views
             BindingContext = model = new SearchViewModel(search);
         }
 
-        private void Artist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private async void Artist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            var artistVM = (e.SelectedItem as ArtistViewModel);
+            await Navigation.PushAsync(new ArtistDetailsPage(artistVM.Artist));
 
+            var request = new UserActivityArtistInsertRequest()
+            {
+                ArtistID = artistVM.Artist.ID,
+                InteractedAt = DateTime.Now
+            };
+            await _loggingService.InsertActivityArtist(SignedInUserHelper.User.ID, request);
         }
 
-        private void Album_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private async void Album_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            var albumVM = (e.SelectedItem as AlbumViewModel);
+            await Navigation.PushAsync(new AlbumDetailsPage(albumVM.Album));
 
+            var request = new UserActivityAlbumInsertRequest()
+            {
+                AlbumID = albumVM.Album.ID,
+                InteractedAt = DateTime.Now
+            };
+            await _loggingService.InsertActivityAlbum(SignedInUserHelper.User.ID, request);
         }
 
-        private void Track_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private async void Track_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            var trackVM = (e.SelectedItem as TrackViewModel);
+            await Navigation.PushAsync(new MusicPlayerPage(new List<Track> { trackVM.Track }));
 
+            var request = new UserActivityTrackInsertRequest()
+            {
+                TrackID = trackVM.Track.ID,
+                InteractedAt = DateTime.Now
+            };
+            await _loggingService.InsertActivityTrack(SignedInUserHelper.User.ID, request);
         }
 
-        private void Playlist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        private async void Playlist_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            var selectedPlaylist = (e.SelectedItem as Playlist);
+            await Navigation.PushAsync(new PlaylistDetailsPage(selectedPlaylist));
 
+            var request = new UserActivityPlaylistInsertRequest()
+            {
+                PlaylistID = selectedPlaylist.ID,
+                InteractedAt = DateTime.Now
+            };
+            await _loggingService.InsertActivityPlaylist(SignedInUserHelper.User.ID, request);
         }
     }
 }
